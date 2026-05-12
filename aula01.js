6f1ff6f6b66348094ac1ac9086a6131d792f0510
KIKO TESOURO <joao.schelbauer.cardoso@escola.pr.gov.br>
2026-05-12 10:27:56 -0300

    Add files via upload

---
+++ aula01.js
@@ -0,0 +1,9 @@
+//alert("O carpa campim fica batendo no amiguinho!!!")
+
+console.log("Primeiro código em JS");
+console.log(4+5);
+console.log( 3 * 4 );
+console.log( true );
+console.log( 3.89 );
+
+console.log( "Thiago" + " " +23+" " )
---
+++ aula02.js
@@ -0,0 +1,29 @@
+ //DEFINIÇÃO DE VARIÁVEIS
+
+  let pessoa = "Nome da pessoa";
+  let idadePessoa = 30;
+  let alturaPessoa = 1.93;
+  let statusPessoa = true;
+  let salarioPessoa = 1500,
+  let salarioBonus = 100;
+
+  let salarioTotal = salarioBonus + salarioPessoa;
+
+  console.log( pessoa );
+  console.log( idadePessoa );
+  console.log( alturaPessoa );
+  console.log( statusPessoa );
+  console.log( salarioPessoa );
+  console.log( salarioBonus );
+  console.log( salarioTotal );
+
+  //UNINDO TODAS AS INFORMAÇÕES EM UM TEXTO:
+  console.log(
+  'O nome digitado foi: '+pessoa+
+  ' a idade digitada foi: '+idadePessoa+
+  ' a altura digitada foi: '+alturaPessoa+
+  ' o status da pessoa é: '+statusPessoa+
+  ' o salário da pesssoa é: '+salarioPessoa+
+  ' e o salário bonus é: '+salarioBonus+
+  ' e o salário total é: '+salarioTotal+ '!!!'
+  )
