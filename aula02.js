e2cd269965019bac9a7d11769cdf4fa739569e3a
KIKO TESOURO <joao.schelbauer.cardoso@escola.pr.gov.br>
2026-05-18 09:21:38 -0300

    Add files via upload

--- aula02.js
+++ aula02.js
@@ -4,7 +4,7 @@
   let idadePessoa = 30;
   let alturaPessoa = 1.93;
   let statusPessoa = true;
-  let salarioPessoa = 1500,
+  let salarioPessoa = 1500;
   let salarioBonus = 100;
 
   let salarioTotal = salarioBonus + salarioPessoa;
---
+++ aula03.js
@@ -0,0 +1,20 @@
+let nome = "Thiago";
+
+if (nome == "Thiago"){
+    console.log("Voce digitou o nome correto!")
+}else{
+     console.log("Voce digitou outra coisa...")
+}
+
+let idade = 17;
+ 
+if (idade == 18) {
+    console.log("Voce é praticamente adulto...")
+}else if( idade < 18 ){
+    console.log("Voce ainda é de menor...")
+}else {
+    console.log("Voce é adulto...")
+}
+
+//FAÇA UMA VERIFICAÇÃO COM IF E ELSE
+//DE UMA PESSOA SE ELA PODE OU NÃO PODE VOTAR.
